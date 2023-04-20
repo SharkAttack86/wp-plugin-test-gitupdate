c8919a58bc2e61cd54502a54e95d22caea3b0af1
SharkAttack86 <daniele.pani.86@gmail.com>
2023-04-20 16:56:07 +0200

    Create wp-plugin-test-gitupdate.php

---
+++ wp-plugin-test-gitupdate.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name:       WP Plugin Test Gitupdate
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Daniele Pani
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/plugins/the-basics/
+ * Domain Path:       /languages
+ */
